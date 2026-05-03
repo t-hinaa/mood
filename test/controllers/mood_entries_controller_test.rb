11f634e0133d528365e35692474106c21b597382
Hina <thashi.h99@gmail.com>
2026-05-03 11:44:04 +0900

    感情入力test

---
+++ test/controllers/mood_entries_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class MoodEntriesControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
---
+++ test/models/mood_entry_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class MoodEntryTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
