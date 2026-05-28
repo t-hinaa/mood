9564ea6572af1e0e21b7da218cf581b8232fc6af
Hina <thashi.h99@gmail.com>
2026-05-28 17:16:03 +0900

    fix:誤字削除、文字入れ

--- app/models/mood_entry.rb
+++ app/models/mood_entry.rb
@@ -8,14 +8,14 @@ class MoodEntry < ApplicationRecord
   # 気分レベルに対応する絵文字を返すメソッド
   def mood_emoji
     case mood_level
-    when 1 then ""
-    when 2 then 
-    when 3 then 
-    when 4 then 
-    when 5 then 
-    when 6 then 
-    when 7 then 
-    when 8 then 
+    when 1 then "喜"
+    when 2 then "安心"
+    when 3 then "不安"
+    when 4 then "驚"
+    when 5 then "悲"
+    when 6 then "不快"
+    when 7 then "怒"
+    when 8 then "期待"
     end
   end
 
